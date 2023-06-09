1021bbfc5f295c088c1bb0ccaf93578c3bb7bf94
ravi-konjeti <71940212+ravi-konjeti@users.noreply.github.com>
2023-06-08 19:20:49 -0500

    Day 32 : Data is bought to backend

--- backend.py
+++ backend.py
@@ -4,10 +4,18 @@ API_KEY = "93cd574555991f8d15b0ac5f70d0ad4e"
 
 def get_data(place, forecast_days=None, kind=None):
         url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
-        respone = requests.get(url)
-        data = respone.json()
-        return data
+        response = requests.get(url)
+        data = response.json()
+        filtered_data = data["list"]
+        nr_values = 8*forecast_days
+        filtered_data = filtered_data[:nr_values]
+        if kind == "Temperature":
+            filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+        if kind == "Sky":
+            filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+
+        return filtered_data
 
 
 if __name__=="__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
