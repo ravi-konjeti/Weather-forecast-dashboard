ac6d68846209688d54da1f52a3ce2dfadd6f70d0
ravi-konjeti <71940212+ravi-konjeti@users.noreply.github.com>
2023-06-08 19:54:41 -0500

    Day 32 : imported images and created an end to end weather API

--- backend.py
+++ backend.py
@@ -9,11 +9,6 @@ def get_data(place, forecast_days=None, kind=None):
         filtered_data = data["list"]
         nr_values = 8*forecast_days
         filtered_data = filtered_data[:nr_values]
-        if kind == "Temperature":
-            filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-        if kind == "Sky":
-            filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
-
         return filtered_data
 
 
